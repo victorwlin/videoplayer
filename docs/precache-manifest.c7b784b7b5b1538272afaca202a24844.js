1468f5de2bd73a63f445383b8b47db1110165340
victorwlin <57220247+victorwlin@users.noreply.github.com>
2020-04-13 21:32:03 +0800

    Add files via upload

---
+++ docs/precache-manifest.c7b784b7b5b1538272afaca202a24844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3277cdef986f69f99180b953d6f7b19",
+    "url": "/videoplayer/index.html"
+  },
+  {
+    "revision": "b97dc3b4a5efa905b3db",
+    "url": "/videoplayer/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "ca47c3b9da835d3456c3",
+    "url": "/videoplayer/static/js/2.15c9c51f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/videoplayer/static/js/2.15c9c51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97dc3b4a5efa905b3db",
+    "url": "/videoplayer/static/js/main.938896ba.chunk.js"
+  },
+  {
+    "revision": "203006704bebe08d9671",
+    "url": "/videoplayer/static/js/runtime-main.2634cdc7.js"
+  }
+]);
