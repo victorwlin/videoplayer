9c9129cf839aba9b2ab187501ed788a0fb443122
Victor Lin <linvictorw@gmail.com>
2021-02-24 19:07:16 +0800

    Updates

---
+++ precache-manifest.e5d17a0f621c6bddb79766dc3e90b321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd74a77d2060153343ef17a67f00c00",
+    "url": "/videoplayer/index.html"
+  },
+  {
+    "revision": "6bcdd0c67602ce478056",
+    "url": "/videoplayer/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "f6260b546be985bd5530",
+    "url": "/videoplayer/static/js/2.b52b6023.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/videoplayer/static/js/2.b52b6023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bcdd0c67602ce478056",
+    "url": "/videoplayer/static/js/main.606e3872.chunk.js"
+  },
+  {
+    "revision": "203006704bebe08d9671",
+    "url": "/videoplayer/static/js/runtime-main.2634cdc7.js"
+  }
+]);
